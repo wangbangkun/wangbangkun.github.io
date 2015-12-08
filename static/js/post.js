27e947cd60671af03bd4721245c7e96c82903651
wangbangkun <wangbangkun@21eline.com>
2015-12-08 22:37:03 +0800

    Remove comment area.

--- static/js/post.js
+++ static/js/post.js
@@ -8,7 +8,6 @@
 $(document).ready(function() {
     generateContent();
     share();
-    disqus();
 });
 
 /**
@@ -26,14 +25,4 @@ function generateContent() {
 function share(){
     window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"24"},"share":{}};
     with(document)0[getElementsByTagName("script")[0].parentNode.appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
-}
-
-
-function disqus(){
-    /* * * CONFIGURATION VARIABLES * * */
-    var disqus_shortname = '{{site.disqus_shortname}}';
-
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    document.getElementsByTagName("script")[0].parentNode.appendChild(dsq);
 }
